47fc92782672baf0838246c81ddcfb37782d095b
Victor Bobkov <victorbobkov@gmail.com>
2022-04-20 02:49:42 +0400

    Work with Tuples

--- app.ts
+++ app.ts
@@ -2,11 +2,20 @@
 //     name: string
 //     age: number
 // } = {
-const person = {
+const person: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: [number, string] // tuple type - fixed length array as well as fixed types
+} = {
     name: 'Ben',
     age: 19,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 }
+
+// person.role.push('admin')
+// person.role[1] = 10
 
 let favoriteActivities: string[]
 favoriteActivities = ['Sports']
