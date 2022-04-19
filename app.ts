b9fe10b033a92d54f63eb9f71673395914cd642a
Victor Bobkov <victorbobkov@gmail.com>
2022-04-20 02:40:17 +0400

    Work with Arrays types

--- app.ts
+++ app.ts
@@ -4,10 +4,19 @@
 // } = {
 const person = {
     name: 'Ben',
-    age: 19
+    age: 19,
+    hobbies: ['Sports', 'Cooking']
 }
 
+let favoriteActivities: string[]
+favoriteActivities = ['Sports']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+    // console.log(hobby.map()) // error
+}
 
 // Object types can also be created for nested objects
 const product: {
