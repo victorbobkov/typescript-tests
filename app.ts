7224733e2eb79c7d317d64a25aa00814676d1a4c
Victor Bobkov <victorbobkov@gmail.com>
2022-04-20 02:31:56 +0400

    Work with nested objects

--- app.ts
+++ app.ts
@@ -8,3 +8,23 @@ const person = {
 }
 
 console.log(person.name)
+
+// Object types can also be created for nested objects
+const product: {
+    id: string;
+    price: number;
+    tags: string[],
+    details: {
+        title: string;
+        description: string;
+    }
+} = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+    }
+}
+
