61258c83cb3fe89f81a585ef8461f1eee8b51045
Victor Bobkov <victorbobkov@gmail.com>
2022-04-21 00:24:55 +0400

    Work with Enums

--- app.ts
+++ app.ts
@@ -2,16 +2,34 @@
 //     name: string
 //     age: number
 // } = {
-const person: {
-    name: string,
-    age: number,
-    hobbies: string[],
-    role: [number, string] // tuple type - fixed length array as well as fixed types
-} = {
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string] // tuple type - fixed length array as well as fixed types
+// } = {
+//     name: 'Ben',
+//     age: 19,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+
+// enum assigns labels to numbers
+enum Role {
+    ADMIN,
+    READ_ONLY ,
+    AUTHOR
+}
+
+const person = {
     name: 'Ben',
     age: 19,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 }
 
 // person.role.push('admin')
@@ -44,5 +62,9 @@ const product: {
         title: 'Red Carpet',
         description: 'A great carpet - almost brand-new!'
     }
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log('is author')
 }
 
